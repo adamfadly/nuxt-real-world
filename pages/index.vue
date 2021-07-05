13c1d651129521226b51d7b561db3c798d6505f7
Adam Fadly <adamfadly11@gmail.com>
2021-07-05 20:49:42 +0700

    clean up boiler

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container flex items-center justify-center margin-0 w-full">
+    <h1 class="font-bold text-6xl">Events</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
+
+<style>
+</style>
