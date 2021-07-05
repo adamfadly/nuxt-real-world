43dbc49a7a4868f9727754b6a2a1209f692c839d
Adam Fadly <adamfadly11@gmail.com>
2021-07-05 22:47:01 +0700

    adding create page

---
+++ components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pt-4">
+    <nuxt-link to="/" class="brand text-4xl text-green-900">Real World Events</nuxt-link>
+      <div class="flex items-center justify-center text-base text-green-700 pt-6">
+        <nuxt-link to="/">List |</nuxt-link>
+        <nuxt-link to="/create-event">| Create</nuxt-link>
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+        
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container flex items-center justify-center margin-0 w-full">
-    <h1 class="font-bold text-6xl">Events</h1>
+    <h1 class="font-bold text-2xl">Events</h1>
   </div>
 </template>
 
