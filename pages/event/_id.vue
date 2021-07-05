545e1814614f249e3cec9d41bc4e0ba2950b1c45
Adam Fadly <adamfadly11@gmail.com>
2021-07-05 23:40:17 +0700

    routing view for nuxt

---
+++ pages/event/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 class="text-red-800">Event {{id}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+      head(){
+    return{
+    title: "event "+ this.id,
+    meta: [
+      { 
+        hid: 'description',
+        name: 'description',
+        content: 'you can create a new event in your neighborhood'
+      }
+    ]
+   
+  }
+   }, computed: {
+      id() {
+        return this.$route.params.id
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
